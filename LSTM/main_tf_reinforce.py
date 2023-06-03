f564298fec801195e2bf9b541c5df076780ad286
Dingyi Kang <81428296+Dingyi-Kang@users.noreply.github.com>
2023-06-03 13:23:40 -0500

    Add files via upload

---
+++ LSTM/main_tf_reinforce.py
@@ -0,0 +1,118 @@
+import numpy as np
+import gym
+from reinforce_cnn_tf import PolicyGradientAgent
+from utils import plotLearning
+from gym import wrappers
+import argparse, gym_unrealcv
+import os
+import cv2
+
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter('tracker_view.mp4', fourcc, 20.0, (336, 336))
+
+#preprocess function converts it to grayscale, and make its value between -1 and 1
+def preprocess(observation):
+    new_maxd = 1.0
+    new_mind = -1.0
+    mx_d = 255.0
+    mn_d = 0.0
+
+    obs_range = mx_d - mn_d
+    # Clip the observation --- make sure each pixel value is between 0-255
+    observation = observation.clip(mn_d, mx_d)
+    # Rescale the observation
+    new_observation = ((observation - mn_d) * (new_maxd - new_mind) / obs_range) + new_mind
+    # converts it to grayscale
+    return np.mean(new_observation, axis=2)
+
+def stack_frames(stacked_frames, frame, buffer_size):
+    if stacked_frames is None:
+        #the * operator can be used to "unpack" the list or tuple into separate arguments.
+        #here, frame.shape returns a tuple representing the shape of the frame (for example, (height, width)). So, *frame.shape would unpack this tuple into separate arguments.
+        stacked_frames = np.zeros((buffer_size, *frame.shape))
+        for idx, _ in enumerate(stacked_frames):
+            stacked_frames[idx,:] = frame
+    else:
+        stacked_frames[0:buffer_size-1,:] = stacked_frames[1:,:]
+        stacked_frames[buffer_size-1, :] = frame
+
+    return stacked_frames
+
+if __name__ == '__main__':
+    '''may continue training by making it ture'''
+    load_checkpoint = True#False
+    dir_path = 'tmp/checkpoints'
+    # Check if the directory exists
+    if not os.path.exists(dir_path):
+        # If the directory does not exist, create it
+        print("!!!Successfully created the directory")
+        os.makedirs(dir_path)
+
+    inpSize = 336
+    #num_channels = 3
+    agent = PolicyGradientAgent(ALPHA=0.0001, GAMMA=0.9, n_actions=7, lstm_neurons=256, input_shape=(inpSize, inpSize), channels=1, 
+                                chkpt_dir=dir_path, gpu={'GPU':0})
+    filename = 'score_history_policyGradient.png'
+    print('will use ', filename, ' and ', agent.gpu)
+    if load_checkpoint:
+        agent.load_checkpoint()
+       
+    parser = argparse.ArgumentParser(description=None)
+    parser.add_argument("-e", "--env_id", nargs='?', default='UnrealSearch-RealisticRoomDoor-DiscreteColor-v0', #'UnrealArm-DiscreteRgbd-v0', #'RobotArm-Discrete-v0',
+                        help='Select the environment to run')
+    parser.add_argument("-r", '--render', dest='render', action='store_true', help='show env using cv2')
+    args = parser.parse_args()
+    env = gym.make(args.env_id, resolution=(inpSize,inpSize))
+    env.seed(1)
+    print("Env created")
+
+    score_history = []
+    score = 0
+    num_episodes = 3000
+    stack_size = 4
+    #by far, looks like index of 0 corresponds to the ob and reward of tracker
+    agentIndex = 0
+    for i in range(num_episodes):
+        done = False
+
+        if i % 20 == 0 and i > 0:
+            avg_score = np.mean(score_history[max(0, i-20):(i+1)])
+            print('episode: ', i,'score: ', score, ' average score %.3f' % avg_score)
+            plotLearning(score_history, filename=filename, window=20)
+        elif i >= num_episodes - 100:
+            print('episode: ', i,'score: ', score)
+        observations = env.reset()
+        # env.render()
+        # print(observations.shape) #(2, 336, 336, 3)
+        ## use only the view of the tracker
+        curr_observation = preprocess(observations[agentIndex]) #print('Shape of preprocessedObservation: ', observation.shape)
+        stacked_frames = None
+        stacked_frames = stack_frames(stacked_frames, curr_observation, stack_size)
+        score = 0
+        while not done:
+            action = agent.choose_action(stacked_frames)
+
+            observations, rewards, done, info = env.step(action)
+            #print('Shape of rewards: ', rewards.shape) #-->(2,)
+            reward = rewards[agentIndex]
+            agent.store_transition(curr_observation, action, reward)
+
+            #cv2.imwrite('tracker_view.png', cv2.cvtColor(observations[agentIndex], cv2.COLOR_RGB2BGR))
+            #only record first and last 100 episodes
+            if i < 100 or i >= num_episodes - 100:
+              out.write(observations[agentIndex])
+
+            curr_observation = preprocess(observations[agentIndex])
+            stacked_frames = stack_frames(stacked_frames, curr_observation, stack_size)
+            
+            score += reward
+        score_history.append(score)
+        
+        agent.learn()
+
+        if i % 20 == 0:
+            agent.save_checkpoint()
+    plotLearning(score_history, filename=filename, window=20)
+    env.close()
+    out.release()
+    print("end")
---
+++ LSTM/reinforce_cnn_tf.py
@@ -0,0 +1,98 @@
+import os
+import numpy as np
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.layers import LSTM, TimeDistributed
+
+class PolicyGradientAgent(object):
+    def __init__(self, ALPHA, GAMMA=0.95, n_actions=7, lstm_neurons=256, 
+                 input_shape=(336, 336), channels=1, chkpt_dir="tmp/checkpoints",
+                 gpu={'GPU': 1}):
+        self.lr = ALPHA
+        self.gamma = GAMMA
+        self.n_actions = n_actions
+        self.action_space = [i for i in range(n_actions)]
+        self.input_height = input_shape[0]
+        self.input_width = input_shape[1]
+        self.channels = channels
+        self.state_memory = []
+        self.action_memory = []
+        self.reward_memory = []
+        self.gpu = gpu
+        self.lstm_neurons = lstm_neurons
+        self.model = None
+        self.build_net()
+        self.model.compile(loss='sparse_categorical_crossentropy',
+                           optimizer=RMSprop(lr=self.lr), sample_weight_mode='temporal')
+        self.checkpoint_file = os.path.join(chkpt_dir, 'policy_network.ckpt')
+    
+    def build_net(self):
+        self.model = Sequential([
+            TimeDistributed(Conv2D(filters=32, kernel_size=(5, 5), strides=1, padding='same',
+                   activation='relu'), input_shape=(None, self.input_height, self.input_width, self.channels)),
+            TimeDistributed(MaxPooling2D(pool_size=(2, 2), strides=2)),
+            TimeDistributed(Conv2D(filters=32, kernel_size=(5, 5), strides=1, padding='same',
+                   activation='relu')),
+            TimeDistributed(MaxPooling2D(pool_size=(2, 2), strides=2)),
+            TimeDistributed(Conv2D(filters=64, kernel_size=(4, 4), strides=1, padding='same',
+                   activation='relu')),
+            TimeDistributed(MaxPooling2D(pool_size=(2, 2), strides=2)),
+            TimeDistributed(Conv2D(filters=64, kernel_size=(3, 3), strides=1, padding='same',
+                   activation='relu')),
+            TimeDistributed(MaxPooling2D(pool_size=(2, 2), strides=2)),
+            TimeDistributed(Flatten()),
+            LSTM(units=self.lstm_neurons, activation='tanh', return_sequences=True),
+            TimeDistributed(Dense(units=self.n_actions, activation='softmax'))
+        ])
+        
+    def choose_action(self, observation):
+        observation = np.array(observation).reshape((1, 4, self.input_height,
+                                                     self.input_width,
+                                                     self.channels))
+        #one layer is batch size (only one batch); second layer is time sequence (use the last one)
+        probabilities = self.model.predict(observation)[0][-1]
+        #print(probabilities)
+        action = np.random.choice(self.action_space, p=probabilities)
+
+        return action
+    
+    def store_transition(self, observation, action, reward):
+        self.state_memory.append(observation)
+        self.action_memory.append(action)
+        self.reward_memory.append(reward)
+
+    def learn(self):
+        seqLength = len(self.state_memory)
+        state_memory = np.array(self.state_memory).reshape(
+            (1, seqLength, self.input_height, self.input_width, self.channels))
+        action_memory = np.array(self.action_memory).reshape(1, seqLength, 1)
+        reward_memory = np.array(self.reward_memory)
+
+        G = np.zeros_like(reward_memory)
+        for t in range(len(reward_memory)):
+            G_sum = 0
+            discount = 1
+            for k in range(t, len(reward_memory)):
+                G_sum += reward_memory[k] * discount
+                discount *= self.gamma
+            G[t] = G_sum
+
+        mean = np.mean(G)
+        std = np.std(G) if np.std(G) > 0 else 1
+        G = (G - mean) / std
+
+        G = G.reshape(1, seqLength)
+        self.model.fit(state_memory, action_memory, sample_weight=G, verbose=0)
+
+        self.state_memory = []
+        self.action_memory = []
+        self.reward_memory = []
+
+    def load_checkpoint(self):
+        print('...Loading Checkpoint...')
+        self.model.load_weights(self.checkpoint_file)
+
+    def save_checkpoint(self):
+        print('...Saving Checkpoint...')
+        self.model.save_weights(self.checkpoint_file)
